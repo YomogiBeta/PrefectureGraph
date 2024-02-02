d0694ca941f808f41c9a3e71a9e926942e4aa5d6
YomogiBeta <46161490+YomogiBeta@users.noreply.github.com>
2024-02-02 21:16:28 +0900

    add ui components

---
+++ src/components/CheckBox/index.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import type { StoryObj } from '@storybook/react'
+import CheckBox from './index'
+
+export default {
+  title: 'Components/CheckBox',
+  component: CheckBox,
+  argTypes: {
+    label: { control: 'text' },
+    checked: { control: 'boolean' },
+    disabled: { control: 'boolean' },
+    style: { control: 'object' }
+  }
+}
+
+type Story = StoryObj<typeof CheckBox>
+
+const Template = (args: {
+  label: string
+  checked?: boolean
+  disabled?: boolean
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  style: React.CSSProperties
+}) => <CheckBox {...args} />
+
+export const Default: Story = {
+  args: {
+    label: 'Default'
+  },
+  render: Template
+}
---
+++ src/components/Flex/index.stories.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import type { StoryObj } from '@storybook/react'
+import Flex from './index'
+
+export default {
+  title: 'Components/Flex',
+  component: Flex,
+  argTypes: {
+    justify: {
+      control: {
+        type: 'select',
+        options: [
+          'flex-start',
+          'flex-end',
+          'center',
+          'space-between',
+          'space-around'
+        ]
+      }
+    },
+    align: {
+      control: {
+        type: 'select',
+        options: ['flex-start', 'flex-end', 'center', 'baseline', 'stretch']
+      }
+    },
+    direction: {
+      control: {
+        type: 'select',
+        options: ['row', 'column', 'row-reverse', 'column-reverse']
+      }
+    },
+    wrap: {
+      control: {
+        type: 'select',
+        options: ['nowrap', 'wrap', 'wrap-reverse']
+      }
+    },
+    gap: { control: 'number' },
+    style: { control: 'object' }
+  }
+}
+
+type Story = StoryObj<typeof Flex>
+
+const Template = (args: {
+  justify:
+    | 'flex-start'
+    | 'flex-end'
+    | 'center'
+    | 'space-between'
+    | 'space-around'
+  align: 'flex-start' | 'flex-end' | 'center' | 'baseline' | 'stretch'
+  direction: 'row' | 'column' | 'row-reverse' | 'column-reverse'
+  wrap: 'nowrap' | 'wrap' | 'wrap-reverse'
+  gap: number
+  children: React.ReactNode
+  style: React.CSSProperties
+}) => <Flex {...args} />
+
+const TemplateChildren = () => (
+  <>
+    <div style={{ width: 100, height: 100, background: 'red' }} />
+    <div style={{ width: 100, height: 50, background: 'green' }} />
+    <div style={{ width: 100, height: 100, background: 'blue' }} />
+  </>
+)
+
+export const Default: Story = {
+  args: {
+    justify: 'flex-start',
+    align: 'flex-start',
+    direction: 'row',
+    wrap: 'nowrap',
+    gap: 1,
+    children: <TemplateChildren />,
+    style: {}
+  },
+  render: Template
+}
